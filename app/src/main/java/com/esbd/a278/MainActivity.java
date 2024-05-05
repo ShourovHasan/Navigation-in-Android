a384d16dacee48a806b5f547c61bdff373e92a75
ShourovRootNext <shourov.hasan@rootnext.com>
2024-05-06 02:38:24 +0600

    android with Navigation

---
+++ app/src/main/java/com/esbd/a278/MainActivity.java
@@ -0,0 +1,14 @@
+package com.esbd.a278;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
